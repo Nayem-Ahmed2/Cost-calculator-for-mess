039bcf27e7c2407163dc2e4cad18c1aa50ac53e8
Nayem-Ahmed2 <ahmed.nayem26639@gmail.com>
2026-08-15 21:50:02 +0600

    Add cost calculator for mess expenses

---
+++ Cost-calculator-for-mess.py
@@ -0,0 +1,6 @@
+Rent=int(input("Enter the mess rent= "))
+Food=int(input("Enter the food cost= "))
+Electricity=int(input("Enter the total electricity bill= "))
+Person=int(input("Enter the number of persons living in the mess= "))
+Output= (Rent +Food + Electricity )//Person
+print("Each person will pay=",Output)
